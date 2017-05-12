68e9e4a8c289ecc570ead7691c295083b60c81c2
Emre Can Eskimez <dot357@users.noreply.github.com>
2017-05-13 00:31:31 +0300

    Add files via upload

--- tipA.py
+++ tipA.py
@@ -3,6 +3,9 @@ import json
 import re
 import os 
 import errno
+import time
+import codecs
+from django.utils.encoding import smart_str       #smart_str() unicode error fix
 
 consumer_key = "40o1vq09rAl5waxx7W23W9MSR"
 consumer_secret = "EoGTClhokJVVDI1N4dNlKSKuJfRckr2mlo2h4cQiAkK6boB2zK"
@@ -10,6 +13,7 @@ access_token = "834870669900935168-2I2yo4sWJIKTARj1i6M54URTYh4yn9A"
 access_token_secret = "R2NsvcPMKO2Z5Ht8MpFU2G12QWNVZ18JmVdQwpohschRf"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+current_date = time.strftime("%d-%m-%Y")
 
 api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
@@ -30,6 +34,14 @@ def mkdir_p(path): 															        #this function checks if directory exi
             pass
         else:
             raise
+			
+def txt(path): 															        #creates txt file at certain path
+    try:
+        path = open(path,'r')
+    except:
+		# if file does not exist, create it
+		path = open(path,'w')
+            
 
 
 
@@ -41,7 +53,7 @@ mkdir_p(path)
 
 start_account_name = raw_input('Enter the starting point')       #with this method we got the starting point as string
 
-path = "/root/origins/" + start_account_name                                                   #setting up the path for starting point
+path = "/root/origins/" + start_account_name + "/" + current_date                                                  #setting up the path for starting point
 mkdir_p(path)	                                                                               #calling the function for new path which is the users dir
 
 #getting start_account's data 
@@ -49,31 +61,29 @@ mkdir_p(path)	                                                                  
 
 start_account_detail = api.get_user(start_account_name)          #this gets the data 
 start_account_followers = tweepy.Cursor(api.followers, start_account_name).items(100)  	#this gets followers for the user
+start_account_friends = tweepy.Cursor(api.friends, start_account_name).items(100)
+		
 print('Screen_name'+ ' ' + 'id')
 print(start_account_detail.screen_name,start_account_detail.id)  
 
 
 
 #FOLLOWERS LIST START
-print(start_account_detail.screen_name)
-print('has')
-print(start_account_detail.followers_count )
-print('followers')
-print(start_account_name + "'s followers are:" )
-
-
-
+print(" Process started" )
 #break time 12.05.2017 16.22
    #todo list : fix tweets protected or not then put datas on text file.after figure out something for discover algh
    
 
 
-
+print(current_date + " Daily followers added to the followers.txt" )
 
 			
 if start_account_detail.protected != True:	    	#FIXED TWEET PROTECT 12.05.2017-20.31||finding protected account is quite tricky tried 3-4 methods nothing worked so i downloaded user data and found Protected = True so i go to that now if account is protected it shows.
+	fn = path + "/" + "followers.txt"				#this is the followers path
+	txt(fn)
+	file = open(fn ,"w")
 	for followers in start_account_followers:                    
-		print followers.screen_name					#i tried making a dir for every follower but it needs space and cpu to run so i will go with txt for every friend and follower also likes 
+		file.write(followers.screen_name + "\n")					#i tried making a dir for every follower but it needs space and cpu to run so i will go with txt for every friend and follower also likes 
 		
 		
 else:
@@ -81,18 +91,45 @@ else:
 
  
 #FOLLOWERS LIST END
-#details we need on general information list
-print(start_account_detail.profile_image_url)
-print(start_account_detail.id)
-print(start_account_detail.screen_name)
-print(start_account_detail.name)
-print(start_account_detail.description)
-print(start_account_detail.lang)
-print(start_account_detail.time_zone)
-print(start_account_detail.friends_count)
-print(start_account_detail.followers_count)
-print(start_account_detail.favourites_count)
 
+#FRIENDS LIST START
+print(current_date + " Daily friends added to the friends.txt" )
+if start_account_detail.protected != True:	  
+	fn = path + "/" + "friends.txt"
+	txt(fn)
+	file = open(fn ,"w")
+	for friends in start_account_friends:                    
+		file.write(friends.screen_name + "\n")					
+		
+		
+else:
+	print(start_account_name + 'is protected')   
+
+
+#FRIENDS LIST STOP
+
+
+#details we need on general information list
+
+fn = path + "/" + "generalinfo.txt"
+txt(fn)
+file = open(fn ,"w")
+
+
+file.write(str(start_account_detail.id) + "\n")
+file.write(start_account_detail.screen_name + "\n")
+file.write(str(start_account_detail.protected) + "\n")
+file.write(smart_str(start_account_detail.description) + "\n")
+file.write(str(start_account_detail.lang) + "\n")
+file.write(str(start_account_detail.time_zone) + "\n")
+file.write(str(start_account_detail.statuses_count) + "\n")
+file.write(str(start_account_detail.friends_count) + "\n")
+file.write(str(start_account_detail.followers_count) + "\n")
+file.write(str(start_account_detail.favourites_count) + "\n")
+file.write(start_account_detail.profile_image_url + "\n")
+
+print("Data is proccessing ... ")
+print('Data is processed in to generalinfo.txt')
    
    
   
